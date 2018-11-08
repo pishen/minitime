7ebb07d7f152db740d48ef62f99dae8c2978a965
Pishen Tsai <pishen02@gmail.com>
2018-11-08 13:14:46 +0800

    fix the subtraction between time points

--- src/main/scala/minitime/Subtract.scala
+++ src/main/scala/minitime/Subtract.scala
@@ -1,6 +1,7 @@
 package minitime
 
 import java.time._
+import java.time.temporal.ChronoUnit
 
 trait Subtract[L, R, C] {
   def apply(l: L, r: R): C
@@ -12,20 +13,22 @@ object Subtract {
   }
 
   implicit val ldp = create((l: LocalDate, r: Period) => l minus r)
-  implicit val ldld = create((l: LocalDate, r: LocalDate) => r until l)
+  implicit val ldld = create(
+    (l: LocalDate, r: LocalDate) => ChronoUnit.DAYS.between(r, l)
+  )
   implicit val ldtp = create((l: LocalDateTime, r: Period) => l minus r)
   implicit val ldtd = create((l: LocalDateTime, r: Duration) => l minus r)
   implicit val ldtldt = create(
-    (l: LocalDateTime, r: LocalDateTime) => Duration.between(l, r)
+    (l: LocalDateTime, r: LocalDateTime) => Duration.between(r, l)
   )
   implicit val ltd = create((l: LocalTime, r: Duration) => l minus r)
   implicit val ltlt = create(
-    (l: LocalTime, r: LocalTime) => Duration.between(l, r)
+    (l: LocalTime, r: LocalTime) => Duration.between(r, l)
   )
   implicit val zdtp = create((l: ZonedDateTime, r: Period) => l minus r)
   implicit val zdtd = create((l: ZonedDateTime, r: Duration) => l minus r)
   implicit val zdtzdt = create(
-    (l: ZonedDateTime, r: ZonedDateTime) => Duration.between(l, r)
+    (l: ZonedDateTime, r: ZonedDateTime) => Duration.between(r, l)
   )
   implicit val pp = create((l: Period, r: Period) => l minus r)
   implicit val dd = create((l: Duration, r: Duration) => l minus r)
