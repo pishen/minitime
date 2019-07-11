7510878fb57b44bbadc1ab0eeb3d068929ab8188
Jimin Hsieh <jimin.hsieh.engineer@gmail.com>
2019-07-11 11:25:53 +0800

    DRY at unit testing

--- minitime/src/test/scala/minitime/PeriodSpec.scala
+++ minitime/src/test/scala/minitime/PeriodSpec.scala
@@ -4,14 +4,16 @@ import org.scalatest.FlatSpec
 
 class PeriodSpec extends FlatSpec {
 
-  "minitime.Period" should "create java.time.Period" in {
+  behavior of "minitime.Period"
+
+  it should "create java.time.Period" in {
     val period = Period(2019, 6, 21)
     assert(period.getYears == 2019)
     assert(period.getMonths == 6)
     assert(period.getDays == 21)
   }
 
-  "minitime.Period" should "parse ISO-8601 period formats' string" in {
+  it should "parse ISO-8601 period formats' string" in {
     val period = Period.parse("P1Y2M3D")
     assert(period.getYears == 1)
     assert(period.getMonths == 2)
